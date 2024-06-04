d1f2e8e1cb0e527deee700656f533b54cfe8fd7f
itsdestroyer0 <120897603+itsdestroyer0@users.noreply.github.com>
2024-06-04 23:39:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/TechPiroBots")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/PhiloMusicSupport")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
@@ -78,10 +78,10 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://te.legra.ph/file/25efe6aa029c6baea73ea.jpg"
+    "START_IMG_URL", "https://graph.org/file/bd105c352227fb18a7cb5.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://te.legra.ph/file/b8a0c1a00db3e57522b53.jpg"
+    "PING_IMG_URL", "https://graph.org/file/a65a3f6380f12556853a1.jpg"
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
 STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
